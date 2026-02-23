57f35afdb01addfc5e8a5d2745115b8d0b16bd4a
Ravikumarmalhotra <ravikumarmalhotra343@gmail.com>
2026-02-24 00:10:52 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'kkso-kbga-v1';
+const urlsToCache = [
+  '/KKSO-KBGA-APP/',
+  '/KKSO-KBGA-APP/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
